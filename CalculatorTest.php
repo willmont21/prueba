7390e04e8a9e35ccc0523a62229091e5d5c7457c
willmont21 <willmont66@gmail.com>
2017-07-07 12:44:20 -0500

    Create CalculatorTest.php

---
+++ CalculatorTest.php
@@ -0,0 +1,11 @@
+ <?php
+require 'Calculator.php';
+ 
+class CalculatorTests extends PHPUnit_Framework_TestCase
+{
+    private $calculator;
+ public function testAdd()
+    {
+        $this->assertEquals(3, $result);
+    }
+        }
